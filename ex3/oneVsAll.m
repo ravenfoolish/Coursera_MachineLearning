7dda7a066b79add9f9daa8d8c41125e9a06d4110
testuser <hifumi_miyoidu@yahoo.co.jp>
2021-09-01 15:40:23 +0900

    modified oneVsAll.m

--- ex3/oneVsAll.m
+++ ex3/oneVsAll.m
@@ -51,7 +51,10 @@ X = [ones(m, 1) X];
 
 initial_theta = zeros(n + 1, 1);
 options = optimset('GradObj', 'on', 'MaxIter', 50);
-
+for i = 1:num_labels
+     [all_theta] = ...
+         fmincg (@(t)(lrCostFunction(t, X, (y == i), lambda)), ...
+                 initial_theta, options);
 
 
 
